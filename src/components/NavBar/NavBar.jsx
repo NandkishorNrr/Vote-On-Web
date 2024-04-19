342563ad55fbd199818c712287046f1547b825e2
Nandkishor Rathore <nrr.tvans@gmail.com>
2024-04-19 23:05:31 +0530

    Set up project structure, create NavBar component, and integrate Tailwind CSS for styling.

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+// components/NavBar/NavBar.jsx
+import React from "react";
+
+function NavBar() {
+  return <nav className="navbar">
+    {/* Add navigation links if needed */}
+    </nav>;
+}
+
+export default NavBar;
